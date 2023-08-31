1c384649f4add1ef8e10b8e8e9a45be41332f914
Devin Garcia <devinmgarcia@gmail.com>
2023-08-31 00:07:39 -0400

    adding contact button

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -3,8 +3,16 @@ import "./Content.css";
 
 export const Content = () => {
   return (
-    <div className="nameWrapper">
+    <>
+    <div className="wrapper-full">
       <div className="name">Hi, I'm Devin.</div>
     </div>
+    <div className="wrapper-full">
+      <div className="title">Full Stack Web Developer</div>
+    </div>
+    <div className="wrapper-full">
+      <a className="title" href="mailto:devinmgarcia@gmail.com">Contact Me</a>
+    </div>
+    </>
   );
 };
